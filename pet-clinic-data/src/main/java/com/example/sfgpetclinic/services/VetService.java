0fe4f55af06723fbdb21c88f2923d08ff725241c
srijibbose <srijibbose.gmail.com>
2021-06-21 16:47:27 +0530

    crud service added

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/VetService.java
@@ -5,11 +5,5 @@ import com.example.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long>{
 }
